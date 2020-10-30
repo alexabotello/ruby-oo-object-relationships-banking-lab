9d9256b04a11b48c9a9963353025d59b88a1c66c
alexabotello <abotello20@yahoo.com>
2020-10-29 20:14:37 -0500

    commit tree 9ea5710cc1f27bfcf1d947002965aadaf7a440d5

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,41 @@
 class BankAccount
+    attr_accessor :balance, :status
 
+    def initialize(name, balance = 1000, status = "open")
+        @name = name 
+        @balance = balance  
+        @status = status
+    end 
+
+    def name
+        @name 
+    end 
+
+    def balance 
+        @balance
+    end 
+
+    def status 
+        @status 
+    end 
+
+    def deposit(new_balance)
+        @balance = @balance + new_balance
+    end 
+
+    def display_balance
+         "Your balance is $#{self.balance}."
+    end 
+
+    def valid?
+        if self.balance > 0 && status == "open"
+            true
+        else 
+            false 
+        end 
+    end 
+    
+    def close_account
+        self.status = "closed"
+    end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
-end
+
+attr_accessor :balance, :status, :bank_account
+
+  def initialize(sender, receiver, amount, status="pending")
+    @receiver = receiver
+    @sender = sender
+    @amount = amount
+    @status = status
+  end 
+
+  def receiver
+    @receiver
+  end 
+
+  def sender
+    @sender
+  end 
+
+  def amount
+    @amount
+  end 
+
+  def status
+    @status
+  end 
+  
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end 
+
+  def execute_transaction
+    self.status = "complete"
+  end 
+end 
+
